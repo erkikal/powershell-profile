7418d972a69fe285b620950b902cfd71fcb9971d
Erki Kaljapulk <erki@kaljapulk.ee>
2024-02-12 18:20:12 +0000

    Fix powershell elevate function path

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -95,9 +95,9 @@ function dirs {
 function admin {
     if ($args.Count -gt 0) {   
         $argList = "& '" + $args + "'"
-        Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
+        Start-Process "$psHome\pwsh.exe" -Verb runAs -ArgumentList $argList
     } else {
-        Start-Process "$psHome\powershell.exe" -Verb runAs
+        Start-Process "$psHome\pwsh.exe" -Verb runAs
     }
 }
 
